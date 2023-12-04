bb0a36620060e941c007b5990ac136c0f34d53b9
0xshariq <145042629+0xshariq@users.noreply.github.com>
2023-12-04 06:06:17 +0000

    New

--- factorial.c
+++ factorial.c
@@ -1,13 +1,29 @@
-#include<stdio.h>
-int main()
-{
-  int n,i,f;
-  printf("Enter the number:");
-  scanf("%d",&n);
-  f=1;
-  for(i=0;i<=n;i++)
-    {
-      f *= i;
+#include <stdio.h>
+
+// Function to calculate factorial
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
     }
-  printf("The factorial of %d is %d",n,f);
+}
+
+// Main function
+int main() {
+    int num;
+
+    // Input from the user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Check if the number is non-negative
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        // Calculate and print the factorial
+        printf("Factorial of %d is: %d\n", num, factorial(num));
+    }
+
+    return 0;
 }
